2262b9dd8fbe72eacc9d23c2684b8797d3a95a41
Dhruv Mevada <mevadadhruv90@gmail.com>
2018-11-28 20:46:06 -0800

    add test app and download and extract agent zip

--- src/ca-ncore/supply/supply.go
+++ src/ca-ncore/supply/supply.go
@@ -2,8 +2,10 @@ package supply
 
 import (
 	"io"
-
+	"path/filepath"
 	"github.com/cloudfoundry/libbuildpack"
+	"os"
+	"io/ioutil"
 )
 
 type Stager interface {
@@ -22,7 +24,7 @@ type Manifest interface {
 
 type Installer interface {
 	//TODO: See more options at https://github.com/cloudfoundry/libbuildpack/blob/master/installer.go
-	InstallDependency(libbuildpack.Dependency, string) error
+	FetchDependency(libbuildpack.Dependency, string) error
 	InstallOnlyVersion(string, string) error
 }
 
@@ -44,6 +46,69 @@ func (s *Supplier) Run() error {
 	s.Log.BeginStep("Supplying ca-ncore")
 
 	// TODO: Install any dependencies here...
+	
+	err := s.Installer.FetchDependency(libbuildpack.Dependency{Name: "apm", Version: "10.6.0"}, filepath.Join(s.Stager.DepDir(), "apm.zip"))
+	
+	if err != nil {
+		return err
+	}
+
+	// bpDir, err := libbuildpack.GetBuildpackDir()
+
+	// if err != nil {
+	// 	return err
+	// }
+
+// err = libbuildpack.ExtractZip(filepath.Join(s.Stager.DepDir(), "apm.zip"), filepath.Join(s.Stager.DepDir(), "apm"))
+	err = libbuildpack.ExtractZip(filepath.Join(s.Stager.DepDir(), "apm.zip"), filepath.Join(s.Stager.DepDir(), "../../","apm"))
+	//todo delete apm.zip
+
+	if err != nil {
+		return err
+	}
+
+	// err = libbuildpack.CopyFile(filepath.Join(bpDir, "IntroscopeAgent.profile"), filepath.Join(s.Stager.DepDir(), "apm", "content", "wily", "IntroscopeAgent.profile"))
+
+	// err = libbuildpack.CopyFile(filepath.Join(s.Stager.DepDir(), "../../apm/wily/"), filepath.Join(s.Stager.DepDir(), "../../wily/"))
+
+	// err = os.Mkdir(filepath.Join(s.Stager.DepDir(), "profile.d"), 0777)
+
+	// if err != nil {
+	// 	return err
+	// }
+	
+	err = ioutil.WriteFile(filepath.Join(s.Stager.DepDir(), "../../apm.sh"), []byte(`
+			echo "running apm.sh"	
+			export COR_ENABLE_PROFILING="1"
+			export COR_PROFILER="{5F048FC6-251C-4684-8CCA-76047B02AC98}"
+			export COR_PROFILER_PATH_64="/home/vcap/apm/wily/bin/wily.NativeProfiler.so"
+			export com.wily.introscope.AgentProfile="home/vcap/apm/wily/IntroscopeAgent.profile"
+			echo "finished running apm.sh"
+			`), 0666)
+
+	
+	err = s.Command.Execute(filepath.Join(s.Stager.DepDir(), "../../"), os.Stdout, os.Stderr, filepath.Join(s.Stager.DepDir(), "../../", "apm.sh"))
+
+	if err != nil {
+		return err
+	}
+
+	/*
+	err = ioutil.WriteFile(filepath.Join(s.Stager.DepDir(), "bin", "apm_start_wrapper.bat"), []byte(`
+			echo "running apm_start_wrapper.bat"
+			export COR_ENABLE_PROFILING="1"
+			export COR_PROFILER="{5F048FC6-251C-4684-8CCA-76047B02AC98}"
+			export COR_PROFILER_PATH_64="C:\Users\vcap\deps\0\apm\content\wily\bin\wily.NativeProfiler.dll"
+			export com.wily.introscope.AgentProfile="C:\Users\vcap\deps\0\apm\content\wily\IntroscopeAgent.profile"
+			echo "finished running apm_start_wrapper.bat"
+			.cloudfoundry\hwc.exe
+			`), 0666)
+
+	// err = s.Command.Execute(".", os.Stdout, os.Stderr, filepath.Join(s.Stager.DepDir(), "apm.bat"))
+
+	if err != nil {
+		return err
+	}*/
 
 	return nil
 }
